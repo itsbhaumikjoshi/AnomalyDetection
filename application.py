4b422acf2597801225f62f581f7944d3baba1ed2
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2023-02-19 01:52:54 +0530

    created logger

--- application.py
+++ application.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 
 from utils.accessEnv import accessEnv
+from utils.setupLogger import setupLogger
 
 # loading environment variables
 load_dotenv()
+
+logger = setupLogger(__name__, env=accessEnv("ENV"))
